2839cf3c75d99133821092a271c0cc476dafc4da
Sabrina Burch <83458995+sabrinasirens@users.noreply.github.com>
2023-07-27 15:07:59 -0500

    Create guidedfind.sh

---
+++ Rocky/guidedfind.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+  
+sudo groupadd GalaxyTeam;
+sudo touch /tmp/Moon;
+sudo touch /bin/Star;
+sudo touch /var/log/Earth;
+sudo touch /etc/Saturn
+sudo chown :GalaxyTeam /tmp/Moon;
+sudo chown :GalaxyTeam /bin/Star;
+sudo chown :GalaxyTeam /var/log/Earth;
+sudo chown :GalaxyTeam /etc/Saturn
